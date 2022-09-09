d40da3b4544c53672632453acc8b65ceaec77809
Shamayal Syed <shamayalsyed@Shamayals-MacBook-Pro.local>
2022-09-09 16:23:09 -0400

    Update website header and body with stylings and images.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const myMusicList = [
-    "Explorers - The Midnight",
-    "this is me trying - Taylor Swift",
-    "Empty - Yoste",
-    "Memories - Conan Gray",
-    "Running Up That Hill - Kate Bush",
-    "Beat It - Michael Jackson",
-    "Mikrokosmos - BTS",
-    "At Last - Etta James",
-    "Cruel Summer - Taylor Swift",
-    "Vampires - The Midnight",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/5fZ3BmBaq29MYI2Xsb5FBR?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/0Ir4WOwXR5F0Ah2CX1ViUG?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/1OOtq8tRnDM8kG2gqUPjAj?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/0ikz6tENMONtK6qGkOrU3c?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/29d0nY7TzCoi22XBqDQkiP?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/2374M0fQpWi3dLnB54qaLX?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/4y1LsJpmMti1PfRQV9AWWe?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/6yEAAIEHu4GcUFptg5W9kI?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/2HHtWyy5CgaQbC7XSoOb0e?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+    "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/7jwQlYGpOml9ETGre1HqGA?utm_source=generator&theme=0' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
 ];
 
 const mixList = document.querySelector(".my-songs");
@@ -17,21 +17,21 @@ const total = document.querySelector(".text");
 
 //create the remove/hide functionality when clicking the button
 button.addEventListener("click", function () {
-    mySongs(myMusicList);
-    mixList.classList.remove("hide");
-    button.classList.add("hide");
+  mySongs(myMusicList);
+  mixList.classList.remove("hide");
+  button.classList.add("hide");
 });
 
-total.innerText = `My current top ${myMusicList.length} songs 🎧 `;
+total.innerText = `The top ${myMusicList.length} songs of the decade 🎧`;
 
 //function that turns the array into html list elements
-const mySongs = function(songs) {
-    //this is how we are able to access each string in the list we made
-    songs.forEach(function (song, index){
-        let li = document.createElement("li");
-        li.classList.add("song");
-        li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`
+const mySongs = function (songs) {
+  //this is how we are able to access each string in the list we made
+  songs.forEach(function (song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`;
 
-        mixList.append(li);
-    });
+    mixList.append(li);
+  });
 };
